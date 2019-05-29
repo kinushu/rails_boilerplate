92f78b6733bf1bde6d5c42d56e2365434c0ab398
shuhei kinukawa <kinushu@gmail.com>
2019-05-30 01:28:33 +0900

    add root path

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,10 @@
+class WelcomeController < ApplicationController
+
+  def index
+  end
+
+  # def robots
+  #   @projects = Project.all_public.active
+  #   render :layout => false, :content_type => 'text/plain'
+  # end
+end
